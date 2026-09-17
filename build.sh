e497a027b5da8a2a8c9d0a9c79094eb1e1237b56
MichalisMichael26 <mike-rhapsody@hotmail.com>
2026-09-17 11:56:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,26 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if password:
+    user, created = User.objects.get_or_create(username=username)
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+    print('Admin user configured successfully')
+else:
+    print('DJANGO_SUPERUSER_PASSWORD is not set')
+"
